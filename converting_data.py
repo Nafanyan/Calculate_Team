af7be5981686c0074a9edd6ac94ec4a89e049545
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-02-12 13:28:39 +0300

    Сделал convert_data с комплексными числами

--- converting_data.py
+++ converting_data.py
@@ -1,6 +1,29 @@
 import user_interface as ui
 
 def convert():
-    expression = ui.data_input().split()
-    return expression
+    source = ui.data_input()
+    expression = source.split()
+    if ('i' in source or 'j' in source):
+        return convert_complex(expression)
+    else:
+        return convert_rational(expression)
+
+def convert_complex(expr):
+    expr[1] = expr[1] + expr[2]
+    expr[5] = expr[5] + expr[6]
+    expr.pop(2)
+    expr.pop(5)
+    expr = list(map(lambda el: el.replace('i',''),expr))
+    expr[0] = complex(int(expr[0]),int(expr[1]))
+    expr[3] = complex(int(expr[3]), int(expr[4]))
+    expr.pop(1)
+    expr.pop(3)
+    return expr
+
+def convert_rational(expr):
+    expr[0] = int(expr[0])
+    expr[2] = int(expr[2])
+    return expr
+
+
 
