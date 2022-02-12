75abf51ce323dcbaa8f443a76c6c118db0ef6ff4
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-02-12 13:29:39 +0300

    перенёс int в модуль конвертации

--- knot.py
+++ knot.py
@@ -4,8 +4,8 @@ import converting_data as cd
 
 def button():
     sup_arr = cd.convert()
-    one_value = int(sup_arr[0])
-    two_value = int(sup_arr[2])
+    one_value = sup_arr[0]
+    two_value = sup_arr[2]
     cl.init(one_value,two_value)
     result = cl.distribution(sup_arr[1])
     ui.data_viewer(result)
